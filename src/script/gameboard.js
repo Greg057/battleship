ef73ffe6952f0a23ac3e2c72084a0c77e83dfb19
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-21 10:58:50 +0100

    Add already hit logic

--- src/script/gameboard.js
+++ src/script/gameboard.js
@@ -11,7 +11,7 @@ export default class Gameboard {
         for (let i = 0; i < 10; i++) {
             this.board.push([])
             for (let j = 0; j < 10; j++) {
-                this.board[i].push([])
+                this.board[i].push([true])
             }
         }
     }
@@ -27,15 +27,15 @@ export default class Gameboard {
     receiveAttack (coordinates) {
         const row = coordinates[0]
         const column = coordinates[1]
-        if (this.board[row][column] instanceof Ship) {
-            this.board[row][column].hit()
-            return this.board[row][column].timesHit
-            
+        if (this.board[row][column]) {
+            if (this.board[row][column] instanceof Ship) {
+                this.board[row][column].hit()            
+            } else {
+                this.missedHits.push(coordinates)
+            }
+            this.board[row][column] = false
         } else {
-            this.board[row][column] = ["missed"]
-            this.missedHits.push(coordinates)
+            return "already attacked"
         }
-        return this.missedHits
-
     }
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -3,16 +3,16 @@ import Ship from "../script/ship";
 
 test("correctly create gameboard 10x10", () => {
     const newBoard = new Gameboard()
-    expect(newBoard.board).toEqual([[[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []], 
-                                    [[], [], [], [], [], [], [], [], [], []]])
+    expect(newBoard.board).toEqual([[[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], 
+                                    [[true], [true], [true], [true], [true], [true], [true], [true], [true], [true]], ])
     const cruiser = new Ship(3)
     newBoard.placeShip(cruiser, [1, 1])
     /* expect(newBoard.board).toEqual([[[], [], [], [], [], [], [], [], [], []], 
@@ -25,14 +25,12 @@ test("correctly create gameboard 10x10", () => {
                                     [[], [], [], [], [], [], [], [], [], []], 
                                     [[], [], [], [], [], [], [], [], [], []], 
                                     [[], [], [], [], [], [], [], [], [], []]]) */
-    expect(newBoard.receiveAttack([1,1])).toEqual(1)
-    expect(newBoard.receiveAttack([1,2])).toEqual(2)
-    expect(newBoard.receiveAttack([1,3])).toEqual(3)
-    const destroyer = new Ship(3)
-    newBoard.placeShip(destroyer, [2, 4])
-    expect(newBoard.receiveAttack([2,4])).toEqual(1)
-    expect(newBoard.receiveAttack([2,5])).toEqual(2)
-    expect(newBoard.receiveAttack([2,6])).toEqual(3) 
-    expect(newBoard.receiveAttack([2,7])).toEqual(["missed"])
+    newBoard.receiveAttack([1,1])
+    expect(cruiser.timesHit).toEqual(1)
+    expect(newBoard.board[1][1]).toBeFalsy
+    expect(newBoard.receiveAttack([1,1])).toEqual("already attacked")
+    newBoard.receiveAttack([3,1])
+    expect(newBoard.missedHits).toEqual([[3,1]])
+    expect(newBoard.receiveAttack([3,1])).toEqual("already attacked")
     
 })
