19884b697bdb655f2b0ac243a21e18194c1443a6
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-21 19:55:42 +0100

    working on fixing placing ships UI

--- src/script/shipUI.js
+++ src/script/shipUI.js
@@ -2,10 +2,17 @@ export default function loadShipUI (board) {
     const playerBoard = document.querySelector(".player-board")
     const changeRotation = document.querySelector("#change-rotation")
 
+    const boardCells = document.querySelector(".player-board").querySelectorAll(".board-cell")
+
     let rotation = "x"
     changeRotation.addEventListener("click", () => {rotation === "x" ? rotation = "y" : rotation = "x"})
 
     let clicks = 0
+    console.log(boardCells)
+    boardCells.forEach(cell => {
+        cell.addEventListener("mouseover", (event) => hoverUI(ships[clicks], rotation, event))
+        cell.addEventListener("mouseout", (event) => hoverUI(ships[clicks], rotation, event))
+    })
     playerBoard.addEventListener("click", (event) => {
         if (clicks === 5) return
         if (addShip(event, board, ships[clicks], rotation) === false) return
@@ -13,6 +20,30 @@ export default function loadShipUI (board) {
     })
 
 }
+
+function hoverUI (ship, rotation, event) {
+    console.log("hi")
+    for (let i = 0; i < ship.length; i++) {
+        let cell;
+        if (rotation === "x") {
+            if (event.target.id[0] === "0") {
+                cell = document.getElementById(`0${Number(event.target.id) + i}`)
+            } else {
+                cell = document.getElementById(`${Number(event.target.id) + i}`)
+            }
+        } else if (rotation === "y") {
+            cell = document.getElementById(`${Number(event.target.id) + i * 10}`)
+        }
+        if (event.type == 'mouseover') {
+            cell.style.backgroundColor = "red";
+          }
+          if (event.type == 'mouseout') {
+            cell.style.backgroundColor = "black";
+          }
+        
+    } 
+
+} 
 
 function addShip(event, board, ship, rotation) {
     if (board.placeShip(ship, `${event.target.id[0]}`, `${event.target.id[1]}`, rotation) === false) return false
