3fcd477a64ec08f857f10df7c5be52a3326c94dd
Greg057 <88341092+Greg057@users.noreply.github.com>
2024-01-21 18:33:54 +0100

    Add rotation logic

--- src/script/shipUI.js
+++ src/script/shipUI.js
@@ -1,23 +1,31 @@
 export default function loadShipUI (board) {
     const playerBoard = document.querySelector(".player-board")
+    const changeRotation = document.querySelector("#change-rotation")
+
+    let rotation = "x"
+    changeRotation.addEventListener("click", () => {rotation === "x" ? rotation = "y" : rotation = "x"})
 
     let clicks = 0
     playerBoard.addEventListener("click", (event) => {
         if (clicks === 5) return
-        if (addShip(event, board, ships[clicks]) === false) return
+        if (addShip(event, board, ships[clicks], rotation) === false) return
         clicks++
     })
 
 }
 
-function addShip(event, board, ship) {
-    if (board.placeShip(ship, `${event.target.id[0]}`, `${event.target.id[1]}`, "x") === false) return false
+function addShip(event, board, ship, rotation) {
+    if (board.placeShip(ship, `${event.target.id[0]}`, `${event.target.id[1]}`, rotation) === false) return false
     for (let i = 0; i < ship.length; i++) {
         let cell;
-        if (event.target.id[0] === "0") {
-            cell = document.getElementById(`0${Number(event.target.id) + i}`)
-        } else {
-            cell = document.getElementById(`${Number(event.target.id) + i}`)
+        if (rotation === "x") {
+            if (event.target.id[0] === "0") {
+                cell = document.getElementById(`0${Number(event.target.id) + i}`)
+            } else {
+                cell = document.getElementById(`${Number(event.target.id) + i}`)
+            }
+        } else if (rotation === "y") {
+            cell = document.getElementById(`${Number(event.target.id) + i * 10}`)
         }
         cell.classList.add("ship-cell")
     }  
